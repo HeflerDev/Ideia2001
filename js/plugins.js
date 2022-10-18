140c794c83939481898b3dd476cbb5b118657014
HeflerDev <heflerdev@gmail.com>
2022-10-18 18:36:53 -0300

    I2 - Init

---
+++ js/plugins.js
@@ -0,0 +1,24 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+  let method;
+  const noop = function () {
+  };
+  const methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+  ];
+  let length = methods.length;
+  const console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+}());
+
+// Plugins Bellow
